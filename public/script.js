9a43c01f51be8ef0f0cc923d6cf6fc9794a611cf
antonbaranov996KAZAKH <antonbaranov996@gmail.com>
2026-03-17 20:49:07 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,83 @@
+let isGameRunning = false;
+let endTime = null;
 let teamCode = '';
 let tripCounter = 0;
 
 function login() {
   teamCode = document.getElementById('teamCode').value.trim();
   if (!teamCode) return alert('Введите код команды');
+function updateTimer() {
+  if (!isGameRunning || !endTime) {
+    document.getElementById('timer').textContent = "Остановлен";
+    return;
+  }
 
+  const now = Date.now();
+  const diff = endTime - now;
+
+  if (diff <= 0) {
+    isGameRunning = false;
+    document.getElementById('timer').textContent = "00:00";
+    return;
+  }
+
+  const minutes = Math.floor(diff / 60000);
+  const seconds = Math.floor((diff % 60000) / 1000);
+
+  document.getElementById('timer').textContent =
+    `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+function fetchGameStatus() {
+  fetch('/api/status')
+    .then(res => res.json())
+    .then(data => {
+      isGameRunning = data.isRunning;
+      endTime = data.endTime;
+    });
+}
+
+  function goTrip() {
+  if (!isGameRunning) {
+    alert('Игра ещё не началась!');
+    return;
+  }
+
+  const address = document.getElementById('addressInput').value.trim();
+  if (!address) return alert('Введите адрес');
+
+  if (!confirm('Вы действительно хотите совершить поездку?')) return;
+
+  fetch('/api/trip', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ teamCode, address })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (!data.success) return alert(data.info);
+
+    tripCounter++;
+
+    const tripsEl = document.getElementById('tripsLeft');
+    tripsEl.textContent = tripCounter;
+
+    tripsEl.classList.remove('jump');
+    void tripsEl.offsetWidth;
+    tripsEl.classList.add('jump');
+
+    updateHistory(data.tripsHistory);
+    alert(data.info);
+
+    document.getElementById('history-box').scrollTop =
+      document.getElementById('history-box').scrollHeight;
+  });
+}
+// автообновление
+setInterval(() => {
+  fetchGameStatus();
+  updateTimer();
+}, 1000);
+  
   fetch('/api/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -53,6 +126,21 @@ function goTrip() {
   });
 }
 
+function startGame(minutes) {
+  fetch('/api/admin/start', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ minutes })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
+}
+
+function stopGame() {
+  fetch('/api/admin/stop', { method: 'POST' })
+    .then(res => res.json())
+    .then(data => alert(data.message));
+}
 function updateHistory(history) {
   const ul = document.getElementById('tripsHistory');
   ul.innerHTML = '';
